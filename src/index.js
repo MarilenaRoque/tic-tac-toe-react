35b8ac2b27afaf8d645f74b4ab0bb9f3cf09ff16
MarilenaRoque <marilenas@id.uff.br>
2020-12-28 13:22:22 -0300

    Lifting Up squares states

--- src/index.js
+++ src/index.js
@@ -1,92 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-      return <p>The water would boil.</p>;
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square" onClick={()=> this.props.onClick()}>
+          {this.props.value}
+        </button>
+      );
     }
-    return <p>The water would not boil.</p>;
-}
-
-const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-};
-
-function toCelsius(fahrenheit) {
-    return (fahrenheit - 32) *5 /9;
-}
-
-
-function toFahrenheit(celsius) {
-    return (celsius *9 /5) +32;
-}
-
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if (Number.isNaN(input)) {
-        return '';
-    }
-    const output = convert(input);
-    const rounded = Math.round(output * 1000)/ 1000;
-    return rounded.toString();
-}
+  }
   
-class TemperatureInput extends React.Component {
+  class Board extends React.Component {
     constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
     }
 
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value);
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares : squares});
     }
 
+    renderSquare(i) {
+      return <Square value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}/>;
+    }
+  
     render() {
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
-        return (
-        <fieldset>
-            <legend>Enter temperature in {scaleNames[scale]}:</legend>
-            <input value={temperature}
-                    onChange={this.handleChange} />
-        </fieldset>
-        );
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
     }
-}
-
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-        this.state = {temperature: '', scale: 'c'};
-    }
-
-    handleCelsiusChange(temperature) {
-        this.setState({scale:'c', temperature})
-    }
-
-    handleFahrenheitChange(temperature) {
-    this.setState({scale:'f', temperature})
-    }
-
+  }
+  
+  class Game extends React.Component {
     render() {
-        const scale = this.state.scale;
-        const temperature = this.state.temperature;
-        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-        return( 
-            <div>
-                <TemperatureInput scale='c' temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
-                <TemperatureInput scale='f' temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
-                <BoilingVerdict celsius={parseFloat(celsius)} />
-            </div>
-            
-        );
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
     }
-
-}
-ReactDOM.render(<Calculator/>, document.getElementById('root'));
-
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
