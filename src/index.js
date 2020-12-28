90e0b95833bdb24c89f2f3e631a8a0ad2e5b51f9
MarilenaRoque <marilenas@id.uff.br>
2020-12-28 11:39:27 -0300

    Temperature Tutorial Init
    Studying Lift Up State
    Set an example of Temperature calculator

--- src/index.js
+++ src/index.js
@@ -1,73 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-class Square extends React.Component {
+
+
+function BoilingVerdict(props) {
+    if(props.celsius >= 100) {
+        return <p> The water would boil.</p>
+    }
+    return <p> The water would not boil.</p>
+}
+
+
+
+export default class Calculator extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: null,
-        };
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { temperature: ''};
     }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
     render() {
-      return (
-        <button className="square" onClick={()=> this.setState({value:'X'})}>
-          {this.state.value}
-        </button>
-      );
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>
+                    Enter temperature in Celsius:
+                </legend>
+                <input value={temperature} onChange={this.handleChange}/>
+                <BoilingVerdict celsius={parseFloat(temperature)} />
+            </fieldset>
+        );
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+}
+
+ReactDOM.render(<Calculator/>, document.getElementById('root'));
+
